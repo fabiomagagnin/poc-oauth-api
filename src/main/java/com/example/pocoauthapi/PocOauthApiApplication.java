61512b5f374219204ed95ba1e002c6b6bd940154
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-02-07 16:26:10 -0200

    initial project

---
+++ src/main/java/com/example/pocoauthapi/PocOauthApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.pocoauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOauthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOauthApiApplication.class, args);
+	}
+
+}
+
